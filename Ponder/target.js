51ac80dea8420a5eee3cc64b0f7bf09b9a619cd5
zvolmer <zvolmer@byui.edu>
2025-04-30 12:24:43 -0600

    ponder and started hw

---
+++ Ponder/target.js
@@ -0,0 +1,7 @@
+const para1 = document.querySelector("#intro");
+
+para1.style.backgroundColor = "#f2c556";
+
+
+let img_area = document.querySelector("#starship");
+
---
+++ mission/mission.js
@@ -0,0 +1,15 @@
+let selectElem = document.querySelector("select");
+let logo = document.querySelector("img");
+
+selectElem.addEventListener("change", changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == "dark"){
+        // change body to dark
+        // change logo to the new logo
+    } else {
+        // remove dark class from body
+        // change logo back to original logo
+    }
+}
