071e229647b73ecbf49f595b7ae5cbbc8f02c2d9
zvolmer <zvolmer@byui.edu>
2025-05-12 12:29:44 -0600

    added l

--- Ponder/events.js
+++ Ponder/events.js
@@ -4,7 +4,7 @@ function newTask() {
   // get the list element from the DOM
   // get the value entered into the #todo input
   // render out the list 
-  istElement.innerHTML += `
+  listElement.innerHTML += `
     <li> ${task}
       <div>
         <span data-function="delete">❎</span>
